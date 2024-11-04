23b093ea93b20ef6dd94545e267a18e0601b7b3b
Yehan20 <yehansk15@gmail.com>
2024-11-05 01:11:16 +0530

    wip : update on ui layout
    Created the structure of components in the application .Added styling
    to each component.Used bem naming convection .Nesting commponent ui remaining.

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import TextEditor from './TextEditor.vue';
+import Comment from './SingleComment.vue';
 
 </script>
 <template>
-    <div>
-         Comments ...
+    <div class="comments__list__section">
+           <TextEditor/>
+           <Comment/>
+           <Comment/>
+           <Comment/>
+   
     </div>
 </template>
+<style   scoped lang="scss">
+    @import '../assets/mixins.scss';
+    
+    .comments__list__section {
+        @include verticalPadding(10px,0);
+    }
+   
+</style>
---
+++ src/components/TextEditor.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import CommentButton from './CommentButton.vue';
+</script>
+
+<template>
+    <div class="text__editor__container">
+         <h4>Discussion (8 comments)</h4>
+         <textarea name="" id=""  :rows="10" class="text__editor" placeholder="text"></textarea>
+         <div class="text__editor__foter">
+               <p>0/800 Characters</p>
+                <CommentButton/>
+         </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/variables.scss';
+@import '../assets/mixins.scss';
+     .text__editor__container{
+         max-width: 750px;
+          h4 {
+             text-transform: uppercase;
+             color: $darkGray;
+             font-weight: 300;
+             font-size: 14px;
+             margin-bottom: 10px;
+             
+          }
+          .text__editor {
+            
+             width: 100%;
+             padding: 10px;
+             color: $darkGray;
+             
+          }
+     }
+     .text__editor__foter {
+          background-color: $mediumGray;
+          padding: 0 10px;
+          @include flexConfig(space-between,nowrap,center) 
+     }
+</style>
