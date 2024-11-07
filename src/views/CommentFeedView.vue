1f50a70048a66d5c83f608a38f13eedb4f30e2df
Yehan20 <yehansk15@gmail.com>
2024-11-07 23:20:17 +0530

    feat : add prop to text editor to control the comment amount

--- src/views/CommentFeedView.vue
+++ src/views/CommentFeedView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-import CommentSection from '../components/CommentSection.vue';
+import CommentSection from '../components/CommentSection.vue'
 import TextEditor from '../components/TextEditor.vue';
+import { usecommentFeedStore } from '../store/commentFeedStore';
+  
+const commentFeedStore = usecommentFeedStore();
+
 </script>
 <template>
     <div class="comment__feed">
+
+
         <div class="question__wrapper">
+
             <h1>What came first the Egg or the Hen ??</h1>
+            <p>{{commentFeedStore.text}}</p>
+
             <div class="question__detail">
                  <p>I have been wondering what came first some says egg some says hen  
                     my Teacher asked it told me to come up with the answer tommarow even which answer i have i need to prove it 
@@ -16,12 +25,14 @@ import TextEditor from '../components/TextEditor.vue';
                  <!-- Upvote down vote List -->
             </div>
         </div>
-        <TextEditor/>
+        <TextEditor :showDiscussion="true"/>
         <CommentSection/>
     </div>
 </template>
+
+
 <style lang="scss" scoped>
-  @use '../assets/mixins.scss' as m;
+  @use '../assets/scss/mixins.scss' as m;
   
   .comment__feed {
     @include m.verticalPadding(40px,40px);
