cc7e3c93eb9ede15a9165ac820f4f1a81ae98b63
Yehan20 <yehansk15@gmail.com>
2024-11-08 20:03:23 +0530

    feat : add hide /show reply ui and logic

--- src/components/CommentReply.vue
+++ src/components/CommentReply.vue
@@ -1,69 +1,84 @@
 <script setup lang="ts">
 
-import { PxMessageReply ,BiArrowUpCircle,BiArrowDownCircle  }  from "oh-vue-icons/icons";
+import { PxMessageReply, BiArrowUpCircle, BiArrowDownCircle } from "oh-vue-icons/icons";
 import { addIcons } from 'oh-vue-icons';
 import TextEditor from "./TextEditor.vue";
-import { provide, ref } from "vue";
-import { Key} from "../store/messageKey";
-import { usecommentFeedStore } from "../store/commentFeedStore";
+import { ref } from "vue";
+import { usecommentFeedStore } from "../store/commentFeedStore"
+import { Message } from "../types/message";
 
 
+addIcons(PxMessageReply, BiArrowDownCircle, BiArrowUpCircle);
 
-addIcons(PxMessageReply,BiArrowDownCircle,BiArrowUpCircle);
+
 
 // Props
 const props = defineProps<{
-    messageInfo:{
-         id:number,
-         upvoted:boolean,
-         downvoted:boolean,
+    messageInfo: {
+        id: number,
+        upvoted: boolean,
+        downvoted: boolean,
+        amount: number
+        replies: Message[],
     }
 }>()
 
-// Store
-const commentFeedStore  = usecommentFeedStore();
 
-// Provide
-provide(Key, props.messageInfo.id);
+// Store
+const commentFeedStore = usecommentFeedStore();
 
 // refs
 const canReply = ref(false);
+const notCollapsed = ref(props.messageInfo.amount < 4);
 
 //Methods
 const toggleReply = () => void (canReply.value = !canReply.value);
+const toggleCollapse = () => void (notCollapsed.value = !notCollapsed.value);
+
 
 </script>
 <template>
-    <div class="single__comment__reply">
+    <div class="single__comment__reply" :id="props.messageInfo.id.toString()">
 
         <div class="comment__buttons__wrapper">
 
             <button @click="toggleReply">
                 <span></span>
                 <v-icon scale=1 name="px-message-reply"></v-icon>
-                Replys
+                Replies {{ props.messageInfo.amount }}
             </button>
 
-            <button class="btn__upvote" :class="{selected:props.messageInfo.upvoted}" @click="commentFeedStore.upvoteComment(props.messageInfo.id)">
-                 <v-icon scale=1.2 name="bi-arrow-up-circle"></v-icon>
+            <button class="btn__upvote" :class="{ selected: props.messageInfo.upvoted }"
+                @click="commentFeedStore.upvoteComment(props.messageInfo.id)">
+                <v-icon scale=1.2 name="bi-arrow-up-circle"></v-icon>
             </button>
-            <button class="btn__downvote" :class="{selected:props.messageInfo.downvoted}" @click="commentFeedStore.downvoteComment(props.messageInfo.id)">
-                  <v-icon scale=1.2 name="bi-arrow-down-circle"></v-icon>
+            <button class="btn__downvote" :class="{ selected: props.messageInfo.downvoted }"
+                @click="commentFeedStore.downvoteComment(props.messageInfo.id)">
+                <v-icon scale=1.2 name="bi-arrow-down-circle"></v-icon>
             </button>
         </div>
 
 
         <template v-if="canReply">
-            <TextEditor @@hideEditor="toggleReply()" :showDiscussion="false" />
+            <TextEditor :parentId="messageInfo.id" @@hideEditor="toggleReply()" :showDiscussion="false" />
         </template>
 
 
     </div>
-    <slot />
+    <button v-if="!(props.messageInfo.amount < 4)" @click="toggleCollapse">
+        {{ notCollapsed ? 'Hide' : 'Show Thread' }}
+    </button>
+
+
+    <template v-if="notCollapsed">
+        <slot />
+    </template>
+
 
 </template>
 
 <style scoped lang="scss">
+
 @use '../assets/scss/variables.scss' as v;
 
 .single__comment__reply p {
@@ -72,40 +87,42 @@ const toggleReply = () => void (canReply.value = !canReply.value);
     color: v.$darkGray;
     font-weight: 500;
 }
-.comment__buttons__wrapper  {
-     
+
+.comment__buttons__wrapper {
+
     margin-bottom: 5px;
+
     button {
-     cursor: pointer;
-     background-color: v.$white;
-     border-radius: 30px;
-     padding: 4px 7px;
-     border-width: 3px;
-     margin-right: 5px;
+        cursor: pointer;
+        background-color: v.$white;
+        border-radius: 30px;
+        padding: 4px 7px;
+        border-width: 3px;
+        margin-right: 5px;
 
-     &.btn__upvote { 
-        border-bottom-right-radius: 0;
-        border-top-right-radius: 0;
-        margin-right: 0;
-     }
-     
-     &.btn__downvote{
-        border-bottom-left-radius: 0;
-        border-top-left-radius: 0;
-        margin-right: 0;
-     }   
+        &.btn__upvote {
+            border-bottom-right-radius: 0;
+            border-top-right-radius: 0;
+            margin-right: 0;
+        }
 
-     &.btn__upvote.selected {
-         color: v.$red;
-     }
-     
-     &.btn__downvote.selected {
-        color: v.$blue;
-    }
+        &.btn__downvote {
+            border-bottom-left-radius: 0;
+            border-top-left-radius: 0;
+            margin-right: 0;
+        }
 
-     &:hover {
-         opacity: 0.7;
-     }
+        &.btn__upvote.selected {
+            color: v.$red;
+        }
+
+        &.btn__downvote.selected {
+            color: v.$blue;
+        }
+
+        &:hover {
+            opacity: 0.7;
+        }
     }
 }
 </style>
