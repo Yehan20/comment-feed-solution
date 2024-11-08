31d2b12f9247345b0bb144df9741e78bf9118bc2
Yehan20 <yehansk15@gmail.com>
2024-11-08 20:10:53 +0530

    feat : add ui sort most points and reset button and bind it with methods from store

--- src/views/CommentFeedView.vue
+++ src/views/CommentFeedView.vue
@@ -2,7 +2,7 @@
 import CommentSection from '../components/CommentSection.vue'
 import TextEditor from '../components/TextEditor.vue';
 import { usecommentFeedStore } from '../store/commentFeedStore';
-  
+
 const commentFeedStore = usecommentFeedStore();
 
 </script>
@@ -13,36 +13,51 @@ const commentFeedStore = usecommentFeedStore();
         <div class="question__wrapper">
 
             <h1>What came first the Egg or the Hen ??</h1>
-            <p>{{commentFeedStore.text}}</p>
+            <p>{{ commentFeedStore.text }}</p>
 
             <div class="question__detail">
-                 <p>I have been wondering what came first some says egg some says hen  
-                    my Teacher asked it told me to come up with the answer tommarow even which answer i have i need to prove it 
+                <p>I have been wondering what came first some says egg some says hen
+                    my Teacher asked it told me to come up with the answer tommarow even which answer i have i need to
+                    prove it
                     any thoughts?
                 </p>
             </div>
             <div class="question__stats">
-                 <!-- Upvote down vote List -->
+                <!-- Upvote down vote List -->
             </div>
         </div>
-        <TextEditor :showDiscussion="true"/>
-        <CommentSection/>
+        <TextEditor :parentId="null" :showDiscussion="true" />
+
+        <div class="filter__wrapper">
+            <button :class="{ selected: commentFeedStore.isSortByUpvote }" class="btn btn__sort"
+                @click="commentFeedStore.sortByUpvote()">
+                Most Upvoted
+            </button>
+
+            <button :class="{ selected: !commentFeedStore.isSortByUpvote }" class="btn btn__reset"
+                @click="commentFeedStore.resetSort()">
+                 Reset
+            </button>
+        </div>
+
+        <CommentSection />
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-  @use '../assets/scss/mixins.scss' as m;
-  
-  .comment__feed {
-    @include m.verticalPadding(40px,40px);
-  }
-  .question__wrapper {
-      @include m.verticalMargin(0px,20px)
-  }
-  .question__detail {
-     max-width: 750px;
-     line-height: 1.6;
-  }
- 
+@use '../assets/scss/mixins.scss' as m;
+
+.comment__feed {
+    @include m.verticalPadding(40px, 40px);
+}
+
+.question__wrapper {
+    @include m.verticalMargin(0px, 20px)
+}
+
+.question__detail {
+    max-width: 750px;
+    line-height: 1.6;
+}
 </style>
