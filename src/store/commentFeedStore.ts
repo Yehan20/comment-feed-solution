756f1ee8767112e3b1da48ac15de6023e1b9d32e
Yehan20 <yehansk15@gmail.com>
2024-11-09 23:55:31 +0530

    feat  : update actions handlers and update state data

--- src/store/commentFeedStore.ts
+++ src/store/commentFeedStore.ts
@@ -1,100 +1,109 @@
 import { defineStore } from 'pinia';
 import type { LocalStorage, Message } from '../types/message';
-import { helperDownvote, helperUpvote,helperLocalStorage, helperUpdateLocalStorage } from '../utils/messageHelper';
+import { helperDownvote, helperUpvote, helperLocalStorage, helperUpdateLocalStorage } from '../utils/messageHelper';
 import Images from '../assets/images/profileImages';
 
 /*
-   reddit 
+   Upvote | Down Vote Logic based on reddit
 
-   updovte click increase by one
-   updovte unlick click derease by one
+   updvote click increase by one
+   updvote unlick click derease by one
 
    downvote click decrease by one
    downvote unlick increase by one
 
-   upvote clicked and down vote clicked decrease by two
+   upvote  clicked and down vote clicked decrease by two
    downvote clicked and upvote clicked increase by two
 
 */
 
-const commentFeed:Message[]=  [
+const commentFeed: Message[] = [
 
     {
         id: 1,
         message: "from azerbaijan",
         parentId: null,
-        points:7,
-        isUpvoted:false,
-        isDownvoted:true,
-        userName:"Zeus",
-        profilePic:Images.Zeus,
-        createdAt:new Date("2021-11-08T14:30:00")
+        points: 7,
+        isUpvoted: false,
+        isDownvoted: true,
+        userName: "Zeus",
+        profilePic: Images.Zeus,
+        createdAt: new Date("2021-11-08T14:30:00")
+        ,
+        collapsed: false
     },
     {
         id: 2,
         message: "hello world",
         parentId: 1,
-        points:3,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Hades",
-        profilePic:Images.Hades,
-        createdAt:new Date("2024-10-08T14:30:00")
+        points: 3,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Hades",
+        profilePic: Images.Hades,
+        createdAt: new Date("2024-10-08T14:30:00")
+        ,
+        collapsed: false
     },
     {
         id: 3,
         message: "hello world",
         parentId: 2,
-        points:5,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Hestia",
-        profilePic:Images.Hestia,
-        createdAt:new Date("2024-11-01T14:30:00")
+        points: 5,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Hestia",
+        profilePic: Images.Hestia,
+        createdAt: new Date("2024-11-01T14:30:00"),
+        collapsed: false
     },
     {
         id: 4,
         message: "hello world",
         parentId: 3,
-        points:-1,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Appollo",
-        profilePic:Images.Appollo,
-        createdAt:new Date("2024-08-08T04:30:00")
+        points: -1,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Appollo",
+        profilePic: Images.Appollo,
+        createdAt: new Date("2024-08-08T04:30:00"),
+        collapsed:true
     },
     {
         id: 5,
         message: "hello world",
         parentId: 4,
-        points:10,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Athena",
-        profilePic:Images.Athena,
-        createdAt:new Date("2024-05-08T14:30:00")
+        points: 10,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Athena",
+        profilePic: Images.Athena,
+        createdAt: new Date("2024-05-08T14:30:00"),
+        collapsed: false
     },
     {
         id: 6,
         message: "hello world",
         parentId: 5,
-        points:5,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Kronos",
-        profilePic:Images.Kronos,
-        createdAt:new Date("2022-11-08T14:30:00")
+        points: 5,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Kronos",
+        profilePic: Images.Kronos,
+        createdAt: new Date("2022-11-08T14:30:00"),
+        collapsed: false
     },
     {
         id: 7,
         message: "hello world",
         parentId: 6,
-        points:7,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Ariest",
-        profilePic:Images.Aries,
-        createdAt:new Date("2023-11-08T14:30:00")
+        points: 7,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Ariest",
+        profilePic: Images.Aries,
+        createdAt: new Date("2023-11-08T14:30:00"),
+        collapsed: false
     },
 
 
@@ -104,12 +113,13 @@ const commentFeed:Message[]=  [
         id: 8,
         message: "Yehan",
         parentId: null,
-        points:8,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Dog",
-        profilePic:Images.Aries,
-        createdAt:new Date("2023-11-08T14:30:00")
+        points: 8,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Dog",
+        profilePic: Images.Aries,
+        createdAt: new Date("2023-11-08T14:30:00"),
+        collapsed: false
     },
 
 
@@ -118,24 +128,26 @@ const commentFeed:Message[]=  [
         id: 9,
         message: "Nilnaga",
         parentId: null,
-        points:9,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Cat",
-        profilePic:Images.Aries,
-        createdAt:new Date("2023-11-08T14:30:00")
+        points: 9,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Cat",
+        profilePic: Images.Aries,
+        createdAt: new Date("2023-11-08T14:30:00"),
+        collapsed: false
     },
 
     {
         id: 10,
         message: "Don",
         parentId: null,
-        points:10,
-        isUpvoted:false,
-        isDownvoted:false,
-        userName:"Lion",
-        profilePic:Images.Aries,
-        createdAt:new Date("2023-11-08T14:30:00")
+        points: 10,
+        isUpvoted: false,
+        isDownvoted: false,
+        userName: "Lion",
+        profilePic: Images.Aries,
+        createdAt: new Date("2023-11-08T14:30:00"),
+        collapsed: false
     },
 
 ]
@@ -145,107 +157,76 @@ export const usecommentFeedStore = defineStore('comments', {
 
     state: () => {
         return {
-            text: "Zeus is God",
-            commentFeed:[] as Message[],
-            isSortByUpvote:false,    
-            originalFeed:[] as Message[],
+       
+            commentFeed: [] as Message[],
+            isSortByUpvote: false,
+            isLoaded:false
         }
     },
-    getters:{
  
-    },
-    actions:{
+    actions: {
 
-         addComment(comment:Message){
+        addComment(comment: Message) {
 
-              this.commentFeed.push(comment);
-              this.originalFeed.push(comment);
+            this.commentFeed.push(comment);
+            helperUpdateLocalStorage('Q1', {commentFeed: [...this.commentFeed]})
 
-              helperUpdateLocalStorage('Q1',
-                
-                {   commentFeed:[...this.commentFeed],
-                    originalFeed:[...this.originalFeed],
-                    isSortByUpdate:this.isSortByUpvote
-                      
-                })
-           
-         },
-         upvoteComment(id:number) {   
+        },
 
-              let changedComment = this.commentFeed.find((comment)=>comment.id === id) as Message
-              helperUpvote(changedComment);
-              helperUpdateLocalStorage('Q1',
+        upvoteComment(id: number) {
 
-                {   commentFeed:[...this.commentFeed],
-                    originalFeed:[...this.originalFeed],
-                    isSortByUpdate:this.isSortByUpvote
-                      
-                })
-              
-         },
-         downvoteComment(id:number) {
+            let changedComment = this.commentFeed.find((comment) => comment.id === id) as Message
+            helperUpvote(changedComment);
 
-              let changedComment = this.commentFeed.find((comment)=>comment.id === id) as Message  
-              helperDownvote(changedComment);
+            helperUpdateLocalStorage('Q1', { commentFeed: [...this.commentFeed]})
 
-              helperUpdateLocalStorage('Q1',
-                
-                {   commentFeed:[...this.commentFeed],
-                    originalFeed:[...this.originalFeed],
-                    isSortByUpdate:this.isSortByUpvote
-                      
-                })
-         },
+        },
+
+        downvoteComment(id: number) {
+
+            let changedComment = this.commentFeed.find((comment) => comment.id === id) as Message
+            helperDownvote(changedComment);
+
+            helperUpdateLocalStorage('Q1', {commentFeed: [...this.commentFeed]})
+        },
 
 
-         sortByUpvote(){
+        sortByUpvote() {
+             this.isSortByUpvote = true
 
-               let sorted =  [...this.commentFeed].sort((a,b)=>b.points - a.points);
-               this.isSortByUpvote = true
-               this.commentFeed = sorted;
+             helperUpdateLocalStorage('Q1',{isSortByUpdate: this.isSortByUpvote})
 
-               helperUpdateLocalStorage('Q1',
-                
-                {   commentFeed:[...this.commentFeed],
-                    originalFeed:[...this.originalFeed],
-                    isSortByUpdate:this.isSortByUpvote
-                      
-                })
-             
-         },
+        },
 
-         loadFeed() {
-              const fromLocal =  helperLocalStorage("Q1");
+        loadFeed() {
 
-              if(!fromLocal) {
-                this.commentFeed =   [...commentFeed],
-                this.originalFeed =[...commentFeed]
-              
-              }
-              else {
-                 this.commentFeed = fromLocal.commentFeed;
-                 this.originalFeed = fromLocal.originalFeed;
-                 this.isSortByUpvote = fromLocal.isSortByUpdate
-              }
-           
+            const fromLocal = helperLocalStorage("Q1") as LocalStorage | null;
 
-         },
+            if (!fromLocal) {
+                    this.commentFeed = [...commentFeed],
+                    helperUpdateLocalStorage("Q1",{commentFeed:this.commentFeed,isSortByUpdate:this.isSortByUpvote})
 
-         resetSort (){
-             this.isSortByUpvote = false;
-             this.commentFeed = [...this.originalFeed ] 
+            }
+            else {
 
-             helperUpdateLocalStorage('Q1',
-                
-                {   commentFeed:[...this.commentFeed],
-                    originalFeed:[...this.originalFeed],
-                    isSortByUpdate:this.isSortByUpvote
-                      
-                })
-         }
-       
+                this.commentFeed = fromLocal.commentFeed as Message[];
+                this.isSortByUpvote = fromLocal.isSortByUpdate as  boolean;
+            }
+
+            this.isLoaded = true;
+
+
+        },
+
+        resetSort() {
+            this.isSortByUpvote = false;
+
+            helperUpdateLocalStorage('Q1',{isSortByUpdate: this.isSortByUpvote})
+        },
+
+   
     }
-    
+
 
 })
 
