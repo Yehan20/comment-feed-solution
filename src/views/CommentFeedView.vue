a846bcb2c687bae9d950e7dfb2892a754fc8ced5
Yehan20 <yehansk15@gmail.com>
2024-11-09 23:48:11 +0530

    feat : add load comment feed

--- src/views/CommentFeedView.vue
+++ src/views/CommentFeedView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import CommentSection from '../components/CommentSection.vue'
 import TextEditor from '../components/TextEditor.vue';
 import { usecommentFeedStore } from '../store/commentFeedStore';
+import { addIcons } from 'oh-vue-icons';
+import { RiLoader2Line } from 'oh-vue-icons/icons';
+// import BaseModal from '../components/BaseModal.vue';
+
+addIcons(RiLoader2Line);
 
 const commentFeedStore = usecommentFeedStore();
+
+
+
+onMounted(()=>{
+   commentFeedStore.loadFeed();
+})
+
 
 </script>
 <template>
@@ -13,7 +26,6 @@ const commentFeedStore = usecommentFeedStore();
         <div class="question__wrapper">
 
             <h1>What came first the Egg or the Hen ??</h1>
-            <p>{{ commentFeedStore.text }}</p>
 
             <div class="question__detail">
                 <p>I have been wondering what came first some says egg some says hen
@@ -26,7 +38,7 @@ const commentFeedStore = usecommentFeedStore();
                 <!-- Upvote down vote List -->
             </div>
         </div>
-        <TextEditor :parentId="null" :showDiscussion="true" />
+        <TextEditor :parentId="null" :showDiscussion="true" userName="" />
 
         <div class="filter__wrapper">
             <button :class="{ selected: commentFeedStore.isSortByUpvote }" class="btn btn__sort"
@@ -39,9 +51,19 @@ const commentFeedStore = usecommentFeedStore();
                  Reset
             </button>
         </div>
+     
+        <template v-if="commentFeedStore.isLoaded">
+              <CommentSection />
+        </template>
 
-        <CommentSection />
+        <template v-else> 
+            <div class="loader__wraper">
+                <v-icon scale='2' color="red" name="ri-loader-2-line"></v-icon>
+            </div>    
+        </template>
+
     </div>
+    <!-- <BaseModal/> -->
 </template>
 
 
