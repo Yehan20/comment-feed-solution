2995407f30ccb1b51b80612e8163f77fb97682a6
Yehan20 <yehansk15@gmail.com>
2024-11-11 14:46:34 +0530

    feat : add feed status and create methods that listen to emits emitted by upvote buttons

--- src/components/CommentSection.vue
+++ src/components/CommentSection.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-// import { onUpdated } from 'vue';
+
 import { usecommentFeedStore } from '../store/commentFeedStore';
 import CommentList from './CommentList.vue';
 
@@ -8,43 +8,11 @@ import CommentList from './CommentList.vue';
 const commentFeedStore = usecommentFeedStore();
 
 
-// extract the main comments top level
-const comments = (id = null)=>{
-      
-     
-      if(commentFeedStore.isSortByUpvote){
-           const sorted  = [...commentFeedStore.commentFeed].filter((comment)=>comment.parentId  === id).sort((a,b)=>b.points - a.points);
-
-           return sorted;
-      }
-      
-       return commentFeedStore.commentFeed.filter((comment)=>comment.parentId  === id); // top level comments
-
-
-}
-
-// extract all the replies of any level
-const replies = ()=>{
-       
-      if(commentFeedStore.isSortByUpvote){
-
-           const sorted  = [...commentFeedStore.commentFeed].filter((comment)=>comment.parentId  !== null).sort((a,b)=>b.points - a.points);
-          
-          
-           return sorted;
-      }
-
-
-        return commentFeedStore.commentFeed.filter((replyForComment)=>replyForComment.parentId  !== null); // child level comments    
- 
-}
-
-
 
 </script>
 <template>
-    <div class="comments__list__section">
-        <CommentList  :comments="comments()" :allReplies="replies()" />
+    <div class="comments__section">
+        <CommentList  :comments="commentFeedStore.getComments(null)"  />
     </div>
 </template>
 
@@ -52,7 +20,7 @@ const replies = ()=>{
 
 @use '../assets/scss/mixins.scss' as m;
 
-.comments__list__section {
+.comments__section {
     @include m.verticalPadding(10px, 0);
 }
 </style>
