56723dd6752ada42bdc847d3d1280d78702514ca
Yehan20 <yehansk15@gmail.com>
2024-11-11 14:33:55 +0530

    chore : udpate types

--- src/types/message.ts
+++ src/types/message.ts
@@ -1,20 +1,25 @@
-export interface  Message {
-    
-     id:number ;
-     message:string;
-     parentId:number | null;
-     points:number;
-     isUpvoted:boolean;
-     isDownvoted:boolean;
-     createdAt:Date;
-     profilePic:string ;
-     userName:string;
-     collapsed:  boolean;
+export interface Message {
+
+      id: number;
+      message: string;
+      parentId: number | null;
+      points: number;
+      isUpvoted: boolean;
+      isDownvoted: boolean;
+      createdAt: Date;
+      profilePic: string;
+      userName: string;
+      //      collapsed:  boolean;
 
 }
 
 export interface LocalStorage {
-      commentFeed?:Message[];
-      originalFeed?:Message[];
-      isSortByUpdate?:boolean;
+      commentFeed?: Message[];
+      isSortByUpvote?: boolean;
+      askedBy?:string
+      isDownvoted?:boolean
+      isUpvoted?:boolean
+      question?:string
+      questionDesc?:string,
+      totalPoints?:number
 }
