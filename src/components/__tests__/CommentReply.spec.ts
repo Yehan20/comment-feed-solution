7c4940927466d56282b594453103c7869ea117b0
Yehan20 <yehansk15@gmail.com>
2024-11-12 18:00:48 +0530

    fix : resolve build error

--- src/components/__tests__/CommentReply.spec.ts
+++ src/components/__tests__/CommentReply.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterAll, beforeAll } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import { createPinia, setActivePinia } from "pinia";
 import CommentReply from "../CommentReply.vue";
