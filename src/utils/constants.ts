0ef6586c8b69b0b5ffd3f4227e9cbe488070b93e
Yehan20 <yehansk15@gmail.com>
2024-11-13 06:03:24 +0530

    feat(utils) : update util methods add new method in date helper

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,7 @@ import profile from '../assets/images/profiles/user-9.webp'
 
 export const MESSAGE_LIMIT = 400;
 
+export const ANESTORY_LEVEL = 0 ; // Used in Helper class to Start Nesting
 
 export const DEFAULT_PROFILE = {
       name : "John Doe",
